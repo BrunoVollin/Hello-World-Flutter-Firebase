68eb7a027b523c595077f8aa2aa9db603ed5ddda
Bruno <bruno.3av@gmail.com>
2021-11-29 09:27:04 -0300

    debug line removed

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,6 @@ class MyApp extends StatelessWidget {
             return const Text("Error :(");
           }
           if (snapshot.hasData) {
-            print("sdlçcvopsdnjmv");
             print(Firebase.apps);
             return const MyHomePage(title: 'Flutter Demo Home Page');
           }
