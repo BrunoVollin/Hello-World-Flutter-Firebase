1032f4ab6aa45143e5d05a383431d4271d21848f
Bruno <bruno.3av@gmail.com>
2021-12-12 11:36:52 -0300

    added todo update

---
+++ lib/widgets/update_todo.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class UpdateToDo extends StatelessWidget {
+  UpdateToDo({
+    Key? key,
+    required this.id,
+    required this.updateToDo,
+    required this.dueDate,
+  }) : super(key: key);
+
+  var textText = "";
+  var textDueDate = "";
+  var id = "";
+
+  final String dueDate;
+  final Function updateToDo;
+
+  onChangedDueDate(text) => textDueDate = text;
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: const Text("Editar Mensagem"),
+      content: TextField(
+        controller: TextEditingController()..text = dueDate,
+        onChanged: (text) => onChangedDueDate(text),
+        decoration:
+            const InputDecoration(hintText: "Entre com a data da taréfa..."),
+      ),
+      actions: [
+        TextButton(
+          child: const Text("Criar"),
+          onPressed: () {
+            updateToDo(id, textDueDate);
+            Navigator.of(context).pop();
+          },
+        ),
+        TextButton(
+            child: const Text("Cancelar"),
+            onPressed: () {
+              Navigator.of(context).pop();
+            }),
+      ],
+    );
+  }
+}
