7fe6de147ae721de1e08bcfade637943a0d68de0
Bruno <bruno.3av@gmail.com>
2021-12-12 11:36:14 -0300

    added todo created alert

---
+++ lib/widgets/create_todo.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+class CreateToDo extends StatelessWidget {
+  CreateToDo({
+    Key? key,
+    required this.createToDo,
+  }) : super(key: key);
+
+  var textToDo = "";
+  var textDueDate = "";
+
+  final Function createToDo;
+
+
+  onChangedText(text) => textToDo = text;
+  onChangedDueDate(text) => textDueDate = text;
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: const Text("Editar Mensagem"),
+      content: Container(
+        constraints: const BoxConstraints(maxHeight: 100),
+        child: Column(
+          children: [
+            TextField(
+              onChanged: (text) => onChangedText(text),
+              decoration: const InputDecoration(
+                  hintText: "Entre com o Texto da taréfa..."),
+            ),
+            TextField(
+              onChanged: (text) => onChangedDueDate(text),
+              decoration: const InputDecoration(
+                  hintText: "Entre com a data da taréfa..."),
+            ),
+          ],
+        ),
+      ),
+      actions: [
+        TextButton(
+          child: const Text("Criar"),
+          onPressed: () {
+            createToDo(textToDo, textDueDate);
+            Navigator.of(context).pop();
+          },
+        ),
+        TextButton(child: const Text("Cancelar"), onPressed: () {
+          Navigator.of(context).pop();
+        }),
+      ],
+    );
+  }
+}
